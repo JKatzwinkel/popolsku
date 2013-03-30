47e31a81d857c937e57bb5eaee197f204fc0a073
J. Katzwinkel <thor@mintj>
2013-03-30 15:46:41 +0100

    Re-structured class method order and prepared sophisticated word placing algorithm.

--- szukac.py
+++ szukac.py
@@ -29,6 +29,7 @@ class Zagadka:
 		self.kierunki_ukryte=sorted(kierunki)
 		self.gdzie_jest={gloska:[] for gloska in alfabet}
 		self.czestoscie={gloska:10 for gloska in alfabet}
+		self.hidden=[]
 		self.uncovered=[]
 		self.description=[]
 		if title:
@@ -45,13 +46,6 @@ class Zagadka:
 		print "Error: can't retrieve puzzle instance!"
 		return None
 	
-	# returns instance specified by index
-	def get(index):
-		inst=len(Zagadka.instances)
-		if inst>0 and index in range(-inst, inst):
-			return Zagadka.instances[index]
-		return None
-		
 
 	# ukrych slowa
 	def hide(self, words):
@@ -73,35 +67,23 @@ class Zagadka:
 			#print 'Ukryję słowo', word
 			self.ukryc_slowo(word)
 
-	# marks words in uppercase letters
-	def solve(self, word=None):
-		for row in range(self.wysokosc):
-			for col in range(self.szerokosc):
-				gloska, kierunek, part_of = self.gloska_w((col, row))
-				if kierunek > 0 and (word in part_of):
-					self.gloski[row][col] = gloska.upper()
-		if not word:
-			self.uncovered=[word for word in self.slowa]
-		else:
-			self.uncovered+=[word]
-
-	# fill puzzle up with random letters
-	def fill(self):
-		probranges={}
-		counter=0
-		for gloska, prob in self.czestoscie.items():
-			probranges[counter] = gloska
-			counter += prob
-		# fill
-		for rubryka in self.gloski:
-			for i in range(len(rubryka)):
-				if rubryka[i] in ('_', ' '):
-					rnd = randint(0,counter)
-					while not probranges.get(rnd, None):
-						rnd -= 1
-					rubryka[i] = probranges[rnd]
 		
 		
+	def positions(self, word):
+		todo=filter(lambda x: not x in self.hidden, self.slowa)
+		pos={}
+		for row in range(0, self.szerokosc):
+			for col in range(0, self.wysokosc):
+				score = len(self.kierunki_ukryte)
+				for kier in self.kierunki_ukryte:
+					crossing = self.odpowiedni(word, (col, row), kier)
+					score += crossing
+				pos[score] = pos.get(score,[])+[(col,row)]
+		print 'Placing: ', word
+		for score in pos.keys():
+			if score > -3:
+				print '{0}-star spots: {1}'.format(score, pos[score][0][:5])
+		return pos
 		
 	# hide single word
 	def ukryc_slowo(self, slowo): #hide word
@@ -111,6 +93,7 @@ class Zagadka:
 			self.slowa.remove(arg_slow)
 			return
 		slowo=slowo.lower()
+		self.positions(slowo)
 		# sort the word's letters by their overall frequency
 		rzadkoscie=sorted(slowo, 
 			key=lambda gloska:self.czestoscie[gloska])
@@ -154,16 +137,7 @@ class Zagadka:
 		#TODO: remove word from list
 					
 					
-	# place word at given position
-	def pisac(self, slowo, pozycja, kierunek):
-		col, row = pozycja
-		for gl in slowo.lower():
-			self.ukryc_gloske(gl, (col, row), kierunek)
-			self.part_of[row][col] += [slowo]
-			col += kierunek & 1 # move right if lowest bit is set
-			row += kierunek / 2 & 1 # move down if second bit is set
-			col -= kierunek / 4 & 1 # move left if third bit is set
-			row -= kierunek / 8 & 1 # move up if fourth bit is set
+
 
 	# determine whether a word fits at a given position with certain
 	# alignment
@@ -222,6 +196,46 @@ class Zagadka:
 		self.kierunki[row][col] = kierunek
 		self.gloski[row][col] = gloska
 
+	# place word at given position
+	def pisac(self, slowo, pozycja, kierunek):
+		col, row = pozycja
+		for gl in slowo.lower():
+			self.ukryc_gloske(gl, (col, row), kierunek)
+			self.part_of[row][col] += [slowo]
+			col += kierunek & 1 # move right if lowest bit is set
+			row += kierunek / 2 & 1 # move down if second bit is set
+			col -= kierunek / 4 & 1 # move left if third bit is set
+			row -= kierunek / 8 & 1 # move up if fourth bit is set
+		self.hidden+=[slowo]
+			
+	# marks words in uppercase letters
+	def solve(self, word=None):
+		for row in range(self.wysokosc):
+			for col in range(self.szerokosc):
+				gloska, kierunek, part_of = self.gloska_w((col, row))
+				if kierunek > 0 and (word in part_of):
+					self.gloski[row][col] = gloska.upper()
+		if not word:
+			self.uncovered=[word for word in self.slowa]
+		else:
+			self.uncovered+=[word]
+
+	# fill puzzle up with random letters
+	def fill(self):
+		probranges={}
+		counter=0
+		for gloska, prob in self.czestoscie.items():
+			probranges[counter] = gloska
+			counter += prob
+		# fill
+		for rubryka in self.gloski:
+			for i in range(len(rubryka)):
+				if rubryka[i] in ('_', ' '):
+					rnd = randint(0,counter)
+					while not probranges.get(rnd, None):
+						rnd -= 1
+					rubryka[i] = probranges[rnd]
+			
 	# uncover one word
 	def przyklad(self):
 		index=len(self.uncovered)-len(self.slowa)
@@ -382,7 +396,7 @@ def wordset(words, limit):
 		score=lambda x:sum([fits((x+' ')[i:-i-1]) for i in range(0,len(x)/2)])
 		while len(sub)<limit:
 			likes=sorted(words, key=score, reverse=True)
-			while likes[0] in sub or randint(0,10)<5:
+			while likes[0] in sub:
 				likes.pop(0)
 			like=likes[0]
 			sub+=[like]
